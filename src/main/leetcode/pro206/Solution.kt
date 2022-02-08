5e6cde37f48112d54e24da3d6a61df1e7fe6cd3f
minho <lights8615@gmail.com>
2022-02-09 00:16:26 +0900

    feat: pro83, pro206

---
+++ src/main/leetcode/pro206/Solution.kt
@@ -0,0 +1,32 @@
+package pro206
+
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+// https://leetcode.com/problems/reverse-linked-list/
+class Solution {
+    fun reverseList(head: ListNode?): ListNode? {
+        if (head == null) {
+            return null
+        }
+
+        var newHead = head
+
+        while (head.next != null) {
+            val nextNode = head.next
+            head.next = nextNode!!.next
+
+            nextNode.next = newHead
+            newHead = nextNode
+        }
+
+        return newHead
+    }
+}
---
+++ src/main/leetcode/pro83/Solution.kt
@@ -0,0 +1,33 @@
+package pro83
+
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+class Solution {
+    fun deleteDuplicates(head: ListNode?): ListNode? {
+        if (head == null) {
+            return null
+        }
+
+        var node = head
+
+        while (node!!.next != null) {
+            val nextNode = node.next
+            if (node.`val` == nextNode!!.`val`) {
+                node.next = nextNode.next
+            } else {
+                node = nextNode
+            }
+        }
+
+        return head
+    }
+}
