a0455b13af1b9088a9a9b06cb759f3f8f8d10f70
lights93 <lights8615@gmail.com>
2022-05-16 15:13:35 +0900

    leetcode: pro1091

--- src/main/leetcode/pro1091/Solution.kt
+++ src/main/leetcode/pro1091/Solution.kt
@@ -2,6 +2,7 @@ package pro1091
 
 import java.util.*
 
+// https://leetcode.com/problems/shortest-path-in-binary-matrix/
 class Solution {
     fun shortestPathBinaryMatrix(grid: Array<IntArray>): Int {
         var result = 0
@@ -42,3 +43,44 @@ class Solution {
     }
 
 }
+
+//class Solution {
+//    fun shortestPathBinaryMatrix(grid: Array<IntArray>): Int {
+//        val dp = Array(grid.size) {
+//            IntArray(grid.size) { Int.MAX_VALUE }
+//        }
+//
+//        solve(grid, 0, 0, dp, 1)
+//        if(dp[grid.size - 1][grid.size - 1] == Int.MAX_VALUE) {
+//            return -1
+//        }
+//
+//        return dp[grid.size - 1][grid.size - 1]
+//    }
+//
+//    private fun solve(grid: Array<IntArray>, i: Int, j: Int, dp: Array<IntArray>, len: Int) {
+//        if(i < 0 || i >= grid.size || j < 0 || j >= grid.size) {
+//            return
+//        }
+//
+//        if(grid[i][j] == 1) {
+//            return
+//        }
+//
+//        if(len >= dp[i][j] || len >= dp[grid.size - 1][grid.size - 1]) {
+//            return
+//        }
+//
+//        dp[i][j] = len
+//
+//        solve(grid, i + 1, j, dp, len + 1)
+//        solve(grid, i, j + 1, dp, len + 1)
+//        solve(grid, i + 1, j + 1, dp, len + 1)
+//        solve(grid, i - 1, j, dp, len + 1)
+//        solve(grid, i, j - 1, dp, len + 1)
+//        solve(grid, i - 1, j - 1, dp, len + 1)
+//        solve(grid, i + 1, j - 1, dp, len + 1)
+//        solve(grid, i - 1, j + 1, dp, len + 1)
+//    }
+//
+//}
